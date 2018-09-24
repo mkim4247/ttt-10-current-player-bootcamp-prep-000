cf1356f55e05e63c99ed070f73c7fdc3f06d5f34
Michael Kim <m.kim4247@gmail.com>
2018-09-24 15:36:57 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,10 @@
 def turn_count(board)
-  
+  counter = 0 
+  board.each do |player| 
+    if #{player} 
 end
+
+brothers.each do |brother|
+  puts "This is loop number #{counter}"
+  puts "Stop hitting yourself #{brother}!"
+  counter += 1
