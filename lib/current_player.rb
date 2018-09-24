d48bf78e694d86428f363ea3803a41c85177d0b6
Michael Kim <m.kim4247@gmail.com>
2018-09-24 15:47:30 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board)
   counter = 0 
   board.each do |space| 
-    if "#{space}" == "X" || "#{space}" == "O"
+    if space == "X"
+      counter += 1
+    elsif space == "O"
       counter += 1 
     end
     return counter 
